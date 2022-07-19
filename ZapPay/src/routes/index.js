b567355595e5d357d74ed5bb7dad82e0eb650fd0
Arthur Tavares Carvalho <tavares.arthur@ymail.com>
2022-07-19 09:50:30 -0300

    .

--- ZapPay/src/routes/index.js
+++ ZapPay/src/routes/index.js
@@ -2,10 +2,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Welcome from '../pages/Welcome';
 import DetalhesPedidos from '../pages/DetalhesPedidos'
-import Consulta from '../pages/Consulta';
-import SelecionarPagamento from '../pages/SelecionarPagamento';
-import PagamentoPix from '../pages/PagamentoPix';
-import PagamentoCartao from '../pages/PagamentoCartao'
 import pagamentoConcluido from "../pages/PagamentoConcluido/pagamentoConcluido";
 
 const Stack = createNativeStackNavigator();
@@ -24,27 +20,7 @@ export default function Routes(){
             component={DetalhesPedidos}
             options={{ headerShown: false}}
             />
-
-            <Stack.Screen
-            name = "Consulta"
-            component={Consulta}
-            options={{ headerShown: false}}
-            />
-            <Stack.Screen
-            name = "SelecionarPagamento"
-            component={SelecionarPagamento}
-            options={{ headerShown: false}}
-            />
-            <Stack.Screen
-            name = "PagamentoPix"
-            component={PagamentoPix}
-            options={{ headerShown: false}}
-            />
-            <Stack.Screen
-            name = "PagamentoCartao"
-            component={PagamentoCartao}
-            options={{ headerShown: false}}
-            />
+            
             <Stack.Screen
             name = "pagamentoConcluido"
             component={pagamentoConcluido}
